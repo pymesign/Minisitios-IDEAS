c33390cde61c77771c5022898aba600fb85f3548
Pymesign <diegormoreno@gmail.com>
2021-10-21 11:06:57 -0300

    Switch to blog para añadir el bloque footer

--- wp-content/themes/ideas/footer.php
+++ wp-content/themes/ideas/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,31 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ideas' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ideas' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ideas' ), 'ideas', '<a href="http://www.pymesign.com">Diego Moreno</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<a href="<?php echo esc_url(__('https://wordpress.org/', 'ideas')); ?>">
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf(esc_html__('Proudly powered by %s', 'ideas'), 'WordPress');
+			?>
+		</a>
+		<span class="sep"> | </span>
+		<?php
+		/* translators: 1: Theme name, 2: Theme author. */
+		printf(esc_html__('Theme: %1$s by %2$s.', 'ideas'), 'ideas', '<a href="http://www.pymesign.com">Diego Moreno</a>');
+		?>
+	</div><!-- .site-info -->
+	<?php
+	switch_to_blog('1');
+	// Do something
+	echo do_shortcode('[content_block id=46]');
+	restore_current_blog();
+	?>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
