3b8fa4ce9b9b4e577e531b63d55b1ec033cfd8c9
Pymesign <diegormoreno@gmail.com>
2021-10-28 14:35:42 -0300

    Comentamos los custom fields de proyecto

--- wp-content/themes/ideas/sidebar.php
+++ wp-content/themes/ideas/sidebar.php
@@ -8,36 +8,40 @@
  * @package ideas
  */
 
-if (!is_active_sidebar('sidebar-1')) {
+/*if (!is_active_sidebar('sidebar-1')) {
 	return;
-}
+}*/
 ?>
 
 <aside id="secondary" class="widget-area">
-	<!-- en principio los campos específicos del proyecto salen en el sidebar -->
-	<?php if (get_field('titulo', '10')) : ?>
-		<h2><?php the_field('titulo', '10'); ?></h2>
-	<?php endif; ?>
-	<?php if (get_field('descripcion', '10')) : ?>
-		<h3><?php the_field('descripcion', '10'); ?></h3>
-	<?php endif; ?>
-	<?php if (get_field('problematica_atendida', '10')) : ?>
-		<p><?php the_field('problematica_atendida', '10'); ?></p>
-	<?php endif; ?>
-	<?php if (get_field('solucion_tecnologica_aportada', '10')) : ?>
-		<p><?php the_field('solucion_tecnologica_aportada', '10'); ?></p>
-	<?php endif; ?>
-	<?php if (get_field('impacto', '10')) : ?>
-		<p><?php the_field('impacto', '10'); ?></p>
-	<?php endif; ?>
-	<?php if (get_field('imagen_representativa', '10')) : ?>
-		<p><img src="<?php the_field('imagen_representativa', '10'); ?>" alt="Imagen representativa" /></p>
-	<?php endif; ?>
-	<?php
-	switch_to_blog('1');
-	// Llamado a content block
-	echo do_shortcode('[content_block id=49]');
-	restore_current_blog();
-	?>
-	<?php dynamic_sidebar('sidebar-1'); ?>
+	<div class="row">
+		<div class="col-12 banner-container">
+			<!-- en principio los campos específicos del proyecto salen en el sidebar -->
+			<!--<?php if (get_field('titulo', '10')) : ?>
+				<h2><?php the_field('titulo', '10'); ?></h2>
+			<?php endif; ?>
+			<?php if (get_field('descripcion', '10')) : ?>
+				<h3><?php the_field('descripcion', '10'); ?></h3>
+			<?php endif; ?>
+			<?php if (get_field('problematica_atendida', '10')) : ?>
+				<p><?php the_field('problematica_atendida', '10'); ?></p>
+			<?php endif; ?>
+			<?php if (get_field('solucion_tecnologica_aportada', '10')) : ?>
+				<p><?php the_field('solucion_tecnologica_aportada', '10'); ?></p>
+			<?php endif; ?>
+			<?php if (get_field('impacto', '10')) : ?>
+				<p><?php the_field('impacto', '10'); ?></p>
+			<?php endif; ?>
+			<?php if (get_field('imagen_representativa', '10')) : ?>
+				<p><img src="<?php the_field('imagen_representativa', '10'); ?>" alt="Imagen representativa" /></p>
+			<?php endif; ?>-->
+			<?php
+			switch_to_blog('1');
+			// Llamado a content block
+			echo do_shortcode('[content_block id=49]');
+			restore_current_blog();
+			?>
+			<?php dynamic_sidebar('sidebar-1'); ?>
+		</div>
+	</div>
 </aside><!-- #secondary -->
