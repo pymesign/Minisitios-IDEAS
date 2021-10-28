7927ea46dd6eeb5b53d9105840c6d4e0cfe9a7c5
Pymesign <diegormoreno@gmail.com>
2021-10-28 10:01:25 -0300

    Creamos cards.php

---
+++ wp-content/themes/ideas/cards.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ideas
+ */
+
+if (!is_active_sidebar('sidebar-1')) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+	<!-- en principio los campos específicos del proyecto salen en el sidebar -->
+	<?php if (get_field('titulo', '2376')) : ?>
+		<h2><?php the_field('titulo', '2376'); ?></h2>
+	<?php endif; ?>
+	<?php if (get_field('descripcion', '2376')) : ?>
+		<h3><?php the_field('descripcion', '2376'); ?></h3>
+	<?php endif; ?>
+	<?php if (get_field('problematica_atendida', '2376')) : ?>
+		<p><?php the_field('problematica_atendida', '2376'); ?></p>
+	<?php endif; ?>
+	<?php if (get_field('imagen_representativa', '2376')) : ?>
+		<p><img src="<?php the_field('imagen_representativa', '2376'); ?>" alt="Imagen representativa" /></p>
+	<?php endif; ?>
+
+	<?php dynamic_sidebar('sidebar-1'); ?>
+</aside><!-- #secondary -->
