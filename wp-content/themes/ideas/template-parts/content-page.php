cedc5243b839100229a1ca9aef680a13ad889a95
Pymesign <diegormoreno@gmail.com>
2021-10-28 08:49:45 -0300

    Oculta header h1 si no es frontpage

--- wp-content/themes/ideas/template-parts/content-page.php
+++ wp-content/themes/ideas/template-parts/content-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying page content in page.php
  *
@@ -10,9 +11,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<?php if (!is_front_page()) : ?>
+		<header class="entry-header">
+			<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+		</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<?php ideas_post_thumbnail(); ?>
 
@@ -22,28 +25,28 @@
 
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'ideas' ),
+				'before' => '<div class="page-links">' . esc_html__('Pages:', 'ideas'),
 				'after'  => '</div>',
 			)
 		);
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
+	<?php if (get_edit_post_link()) : ?>
 		<footer class="entry-footer">
 			<?php
 			edit_post_link(
 				sprintf(
 					wp_kses(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'ideas' ),
+						__('Edit <span class="screen-reader-text">%s</span>', 'ideas'),
 						array(
 							'span' => array(
 								'class' => array(),
 							),
 						)
 					),
-					wp_kses_post( get_the_title() )
+					wp_kses_post(get_the_title())
 				),
 				'<span class="edit-link">',
 				'</span>'
