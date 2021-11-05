8be92ead5bbaceaace6265cc93f54c80fce14ab3
Pymesign <diegormoreno@gmail.com>
2021-11-05 08:03:15 -0300

    Creamos child del main theme (ideas-lite)

---
+++ wp-content/themes/ideas-lite/template-parts/content-cards.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ideas
+ */
+
+?>
+
+<div class="col-12">
+	<a href="#" class="panel panel-default panel-border-mandarina">
+		<div style="background-image:url(<?php echo get_template_directory_uri(); ?>/assets/img/acceso1.jpg);" class="panel-heading"></div>
+		<div class="panel-body">
+			<h3>Blog</h3>
+			<div class="text-muted">
+				<p>Enterate de todas las novedades sobre nuestro proyecto.</p>
+			</div>
+		</div>
+	</a>
+</div>
+
+<div class="col-12">
+	<a href="#" class="panel panel-default panel-border-cereza">
+		<div style="background-image:url(<?php echo get_template_directory_uri(); ?>/assets/img/acceso2.jpg);" class="panel-heading"></div>
+		<div class="panel-body">
+			<h3>Proyecto</h3>
+			<div class="text-muted">
+				<p>Averigua como se gesto y de que se trata.</p>
+			</div>
+		</div>
+	</a>
+</div>
+
+<div class="col-12">
+	<a href="#" class="panel panel-default panel-border-lavanda">
+		<div style="background-image:url(<?php echo get_template_directory_uri(); ?>/assets/img/acceso3.jpg);" class="panel-heading"></div>
+		<div class="panel-body">
+			<h3>Institucional</h3>
+			<div class="text-muted">
+				<p>Los Equipos, La Escuela, Los Mentores y muchos mas...</p>
+			</div>
+		</div>
+	</a>
+</div>
